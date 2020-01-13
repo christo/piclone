808bc37420392c0673aceed15f7e2acac9ea66a7
Chris Mountford <chris@think.net.au>
2020-01-14 10:03:00 +1100

    tweak

--- download.sh
+++ download.sh
@@ -3,6 +3,7 @@
 echo downloading/updating rpi-clone script
 mkdir -p ~/src
 cd ~/src
-(git clone -q https://github.com/billw2/rpi-clone.git >/dev/null 2>&1 && cd rpi-clone) || (cd rpi-clone && git pull)
+git clone -q https://github.com/billw2/rpi-clone.git >/dev/null 2>&1 || (cd rpi-clone && git pull)
+cd ~/src/rpi-clone
 echo showing mounted disks:
 df |grep -v 'tmpfs'
